89f261e2e081abc21968e9196bd749ec5a227b0a
Ahmed Salah <iamahmedsalah@yahoo.com>
2025-05-22 16:10:16 +0300

    localhost

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
           <Header />
           <div className="flex-1">
             <Routes>
-              <Route path="/dashborad" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/appointments/*" element={<Appointments />} />
               <Route path="/patients" element={<Patients />} />
               <Route path="/settings" element={<Settings />} />
